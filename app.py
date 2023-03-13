bfffde264b90c1d2508f56a5750a2133003249e1
Alan <75879852+alanxpo@users.noreply.github.com>
2023-03-13 02:32:04 -0700

    Se crearon rutas para obtener boxeadores
    Ahora tenemos para obtener boxeadores desde MySQL

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Hola Mundo"
+#Conexión a la base de datos
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="!Vvictory1",
+    database="victoryapi"
+)
 
-@app.route("/bye")
-def adios():
-    return "bye bye bye"
+#Definición de ruta para obtener un boxeador específico
+@app.route('/api/sql/boxeadores/<int:id>')
+def get_boxeador(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM boxeador WHERE id=%s", [id])
+    data = cursor.fetchall()
+    cursor.close()
+    if len(data) == 0:
+        return jsonify({'mensaje': 'No se encontro el boxeador solicitado'})
+    boxeadores = []
+    for row in data:
+        boxeador = {'id': row[0], 'nombre': row[1], 'peso': row[2], 'altura': row[3], 'edad': row[4], 'record': row[5]}
+        boxeadores.append(boxeador)
+    return jsonify(boxeadores)
+
+#Definición de ruta para obtener todos los boxeadores
+@app.route('/api/sql/boxeadores')
+def get_boxeadores():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM boxeador")
+    data = cursor.fetchall()
+    cursor.close()
+    boxeadores = []
+    for row in data:
+        boxeador = {'id': row[0], 'nombre': row[1], 'peso': row[2], 'altura': row[3], 'edad': row[4], 'record': row[5]}
+        boxeadores.append(boxeador)
+    return jsonify(boxeadores)
+
+#Ejecución de la aplicación
+if __name__ == '__main__':
+    app.run()
